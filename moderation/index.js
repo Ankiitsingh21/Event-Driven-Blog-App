d2f594bc6b8ea9bdcba238af926831b403d7fe40
Ankit Singh <ankiitsingh21@gmail.com>
2026-02-25 21:46:37 +0530

    now learning about kubernetes

---
+++ moderation/index.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const axios = require("axios");
+
+
+const app=express();
+app.use(express.json());
+
+app.post("/events",async (req,res)=>{
+        console.log("Event Received", req.body.type);
+        const {type,data} = req.body;
+        // console.log(type,data);
+        if(type==="CommentCreated"){
+                const status=data.content.includes('orange')?'rejected':'approved';
+                await axios.post("http://event-bus-srv:4005/events",({
+                        type:'CommentModerated',
+                        data:{
+                                id:data.id,
+                                postId:data.postId,
+                                status,
+                                content:data.content
+                        }
+                }))
+        }
+        res.send({});
+        // console.log(type,data) = r
+})
+
+
+
+app.listen(4003, () => {
+  console.log('Listening on 4003');
+});
